becb49f35d8c06e62f7bedf88fd75029b346eff2
Zhipeng Han <hanzhipeng9@gmail.com>
2020-12-23 15:07:20 +0800

    bump to 0.0.6
    Signed-off-by: Zhipeng Han <hanzhipeng9@gmail.com>

--- foundation/engine/__init__.py
+++ foundation/engine/__init__.py
@@ -1,4 +1,2 @@
-from __future__ import absolute_import, division, print_function
-
 from .hook import *
 from .runner import *
--- foundation/visualization/__init__.py
+++ foundation/visualization/__init__.py
@@ -1,4 +1,2 @@
-from __future__ import absolute_import, division, print_function
-
 from .colormap import *
 from .visualizer import *
